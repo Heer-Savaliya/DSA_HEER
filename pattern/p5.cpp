8675a4e6fc7da5b3b7840864e10fd9d65edbab2f
Heer Savaliya <heersavaliya65@gmail.com>
2025-04-22 18:01:08 +0530

    commit

--- pattern/p5.cpp
+++ pattern/p5.cpp
@@ -2,20 +2,14 @@
 using namespace std;
 int main(){
 
-    // 1 2 3 4
-    // 1 2 3
-    // 1 2
-    // 1
-
     int n;
     cout << "Enter n : ";
     cin >> n;
-    for(int i=n; i>=1;i--){
-        for(int j =1;j<=i;j++){
-            cout << j<< " "; 
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=1;j--){
+            cout << " * " << " ";
         }
         cout << "\n";
     }
-
     return 0;
 }
