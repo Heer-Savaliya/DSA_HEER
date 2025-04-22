0a90494187c2088abafbb85c212b2abb2593683b
Heer Savaliya <heersavaliya65@gmail.com>
2025-04-22 11:20:36 +0530

    comit

---
+++ Basic/area.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Calculator  dhfghg"; 
+    cout << "Enter your choice : ";
+    cin >> n;
+    switch (n)
+    {
+    case 1:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+    return 0;
+}
