2a0f9cd3831d0bfc4fe4eabde9a89bd9b6aaf441
Heer Savaliya <heersavaliya65@gmail.com>
2025-04-27 17:34:12 +0530

    pass by value and ref

---
+++ function/passByValue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int sum(int a ,int b){
+    a = a+5; // 10
+    b=b+5;   //15
+    return (a+b);   //25
+}
+int main(){
+    int a =5,b=10;
+    cout << sum(a,b) << "\n";
+    cout << "a = "<<a;     // original value not change
+    cout << "b = "<<b;
+}
---
+++ function/sumOfDIgit.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int sumOfDigit(int n){
+    int sum = 0;
+    for(int )
+    return sum;
+}
+int main(){
+    int n ;
+    cout << "ENter n = " << "\n";
+    cin >> n;
+    cout << "Sum of digit = " <<sumOfDigit(n) << "\n";
+    return 0;
+}
