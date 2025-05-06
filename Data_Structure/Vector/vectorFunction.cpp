9f72a033d75dd94a4df1fa236d9b96e2ca318c84
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-06 06:15:32 +0530

    vector functions

--- Data_Structure/Vector/vectorFunction.cpp
+++ Data_Structure/Vector/vectorFunction.cpp
@@ -3,10 +3,16 @@
 using namespace std;
 int main(){
     vector<char> vec={'a','b','c','d','e'};
-    cout << "Size = " << vec.size() << "\n";   // Size = 5 
+    cout << "Original Size = " << vec.size() << "\n";   // Original Size = 5
+    vec.push_back('f');
+    vec.pop_back();  // f deleted
+    cout << "Front value = " << vec.front() << "\n";   // Front value = a
+    cout << "Last value = " << vec.back() << "\n";   // Last value = e
+    cout << "Value at index = " << vec.at(1) << "\n";   // Value = b (If Value not exits then it will give an error)
 
     for(char value : vec){  
-        cout << value << " ";    // a b c d e
+        cout << value << " ";    // a b c d e f
     }
+    cout << "Size = " << vec.size() << "\n"; //Size =6 (New size)
     return 0;
 }
